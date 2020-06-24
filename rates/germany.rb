e36f19ba42b2edb8f8b5dd4757542745a73ff487
Pluk <meneerpluk@gmail.com>
2020-06-24 10:06:59 +0200

    Germany: Temporary reduction of VAT rates (COVID-19)

--- rates/germany.rb
+++ rates/germany.rb
@@ -9,4 +9,16 @@ country do
     rate :standard, 19
   end
 
+  period do
+    effective_from 2020, 7, 1
+    rate :reduced, 5
+    rate :standard, 16
+  end
+
+  period do
+    effective_from 2021, 1, 1
+    rate :reduced, 7
+    rate :standard, 19
+  end
+
 end
